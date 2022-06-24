0c20a91259c5cda231c83cfee78d4168b8df9bfd
Tomo Eguchi <Tomo.Eguchi@noaa.gov>
2022-06-24 15:58:46 -0700

    Updating data extraction... still not completed for older data that require inshore/offshore files

--- Piedras_Blancas_fcns.R
+++ Piedras_Blancas_fcns.R
@@ -5,47 +5,29 @@ library(loo)
 get.all.data <- function(sheet.name.inshore, sheet.name.offshore,
                          col.types.inshore, col.types.offshore,
                          col.names.inshore, col.names.offshore,
-                         Year, dir.name, 
+                         Year, 
                          xls.file.name.inshore, xls.file.name.offshore,
-                         start.time){
+                         start.time, max.shift){
   
   data.inshore <- get.data(Year = Year, 
-                           dir.name = dir.name, 
                            xls.file.name = xls.file.name.inshore, 
                            sheet.name = sheet.name.inshore,
                            col.types = col.types.inshore, 
                            col.names = col.names.inshore, 
-                           area.name = "inshore",
-                           start.time = start.time)
+                           start.time = start.time,
+                           max.shift = max.shift)
   
   data.offshore <- get.data(Year = Year, 
-                            dir.name = dir.name, 
                             xls.file.name = xls.file.name.offshore, 
                             sheet.name = sheet.name.offshore,
                             col.types = col.types.offshore, 
                             col.names = col.names.offshore, 
-                            area.name = "offshore",
-                            start.time = start.time)
+                            start.time = start.time,
+                            max.shift = max.shift)
   
   data.all <- rbind(data.inshore, data.offshore) %>% 
-    filter(!is.na(Date)) %>%
-    filter(!is.na(Event)) %>%
-    filter(!is.na(Shift)) %>%
-    arrange(Date, Minutes_since_T0)
-  
-  data.all %>% 
-    group_by(Date, Shift) %>%
-    summarise(Date = min(Date),
-              Sea_State = max(SeaState, na.rm = T),
-              Vis = max(Vis, na.rm = T),
-              Mother_Calf = sum(Mother_Calf, na.rm = T),
-              Duration = max(Minutes_since_0000, na.rm = T) - 
-                min(Minutes_since_0000, na.rm = T),
-              Time_T0 = min(Minutes_since_T0, na.rm = T),
-              Time_0000 = min(Minutes_since_0000, na.rm = T)) -> data.shift
-  
-  return(list(all = data.all,
-              shift = data.shift))
+    arrange(Date.date, Minutes_since_0000)
+  return(data.all)
 }
 
 
@@ -57,29 +39,47 @@ get.all.data <- function(sheet.name.inshore, sheet.name.offshore,
 
 # T0 is the beginning of the first shift (start.time)
 
-get.data <- function(Year, dir.name, xls.file.name, sheet.name,
-                     col.types, col.names, area.name, start.time,
+get.data <- function(Year, xls.file.name, sheet.name,
+                     col.types, col.names, start.time,
                      max.shift){
   # col.names <- c("Date", "Event", "Time", "Obs. Code", "Sea State", 
   #                "Vis. IN", "Cow  / Calf")
-  
-  data.out <- read_excel(xls.file.name,
-                         sheet = sheet.name,
-                         col_types = col.types) %>% 
-    select(col.names) %>%
-    transmute(Date = .data[[col.names[[1]]]],
-              Event = .data[[col.names[[2]]]],
-              Time = .data[[col.names[[3]]]],
-              Minutes_since_T0 = sapply(Time, FUN = char_time2min, start.time),
-              Minutes_since_0000 = sapply(Time, FUN = char_time2min),
-              Shift = sapply(Minutes_since_0000, FUN = find.shift, max.shift = max.shift),
-              Obs = .data[[col.names[[4]]]],
-              SeaState = .data[[col.names[[5]]]],
-              Vis = .data[[col.names[[6]]]],
-              Mother_Calf = .data[[col.names[[7]]]],
-              Year = Year,
-              Area = area.name)
-  
+  if (length(col.names) < 8){
+    data.out <- read_excel(xls.file.name,
+                           sheet = sheet.name,
+                           col_types = col.types) %>% 
+      select(col.names) %>%
+      transmute(Date = .data[[col.names[[1]]]],
+                Event = .data[[col.names[[2]]]],
+                Time = .data[[col.names[[3]]]],
+                Minutes_since_T0 = sapply(Time, FUN = char_time2min, start.time),
+                Minutes_since_0000 = sapply(Time, FUN = char_time2min),
+                Shift = sapply(Minutes_since_0000, FUN = find.shift, max.shift = max.shift),
+                Obs = .data[[col.names[[4]]]],
+                SeaState = .data[[col.names[[5]]]],
+                Vis = .data[[col.names[[6]]]],
+                Mother_Calf = .data[[col.names[[7]]]],
+                Year = Year,
+                Area = "off")
+     
+  } else {
+    data.out <- read_excel(xls.file.name,
+                           sheet = sheet.name,
+                           col_types = col.types) %>% 
+      select(col.names) %>%
+      transmute(Date = .data[[col.names[[1]]]],
+                Event = .data[[col.names[[2]]]],
+                Time = .data[[col.names[[3]]]],
+                Minutes_since_T0 = sapply(Time, FUN = char_time2min, start.time),
+                Minutes_since_0000 = sapply(Time, FUN = char_time2min),
+                Shift = sapply(Minutes_since_0000, FUN = find.shift, max.shift = max.shift),
+                Obs = .data[[col.names[[4]]]],
+                SeaState = .data[[col.names[[5]]]],
+                Vis = .data[[col.names[[6]]]],
+                Mother_Calf = .data[[col.names[[7]]]],
+                Year = Year,
+                Area =  .data[[col.names[[8]]]])
+  }
   # Some files contain wrong years...
   years <- year(data.out$Date)
   dif.years <- sum(years - Year, na.rm = T)
@@ -87,6 +87,19 @@ get.data <- function(Year, dir.name, xls.file.name, sheet.name,
     year(data.out$Date) <- Year
   } 
 
+  # filter out rows with NAs in certain fields, then order them using
+  # Date and time since T0 (i.e., start.time defined above).
+  # Create Date fields with character and date format - may not be necessary?
+  data.out %>% 
+    filter(!is.na(Date)) %>%
+    filter(!is.na(Event)) %>%
+    filter(!is.na(Shift)) %>%
+    arrange(Date, Minutes_since_0000) %>%
+    mutate(Date.date = as.Date(Date),
+           Date.char = as.character(Date)) %>%
+    select(-Date) %>%
+    relocate(Date.date) -> data.out
+  
   return(data.out)  
 }
 
@@ -201,18 +214,22 @@ find.shift <- function(x0, max.shift = 4){
 #4 = GRAY WHALE SIGHTING
 #5 = END EFFORT
 #6 = OTHER SPECIES SIGHTING
-
-find.effort <- function(x, max.shift){
+# provide a data frame that came back from get.data function, hrs for 
+# start and end of each day (default = 7 and 19, correspond to 0700
+# and 1900), and the maximum number of shift per day (default to 4).
+find.effort <- function(x, start.hr = 7, end.hr = 19, max.shift = 4){
   # this turns dates in to character
   all.dates <- unique(x$Date.date)
   
   if (max.shift == 4){
     shifts <- c("1", "2", "3", "4")
-    shifts.1 <- c("1/2", "2/3", "3/4", NA)
+
   } else if (max.shift == 5){
     shifts <- c("1", "2", "3", "4", "5")
-    shifts.1 <- c("1/2", "2/3", "3/4", "4/5", NA)
   }
+  
+  start.shift <- seq(start.hr*60, (end.hr*60-180), by = 180)
+  end.shift <- seq((start.hr*60 + 180), (end.hr*60), by = 180)
   
   out.df <- data.frame(Date.char = rep(all.dates,
                                        each = length(shifts)),
@@ -226,48 +243,61 @@ find.effort <- function(x, max.shift){
                        Time_0000 = NA)
   
   for (d in 1:length(all.dates)){
+    # pick just one day's worth of data
     one.day <- filter(x, Date.char == all.dates[d])
-    shifts.one.day <- unique(one.day$Shift)
-    shifts.one.day <- shifts.one.day[-grep("/", shifts.one.day)]
-    for (k in 1:length(shifts.one.day)){
-      shifts.1.one.day <- shifts.1[str_detect(shifts.1, shifts.one.day[k])]
-      if (as.numeric(shifts.one.day[k]) < max.shift){
-        if (as.numeric(shifts.one.day[k]) > 1){
-          one.shift <- one.day %>% filter(Shift == shifts.one.day[k] | 
-                                            Shift == shifts.1.one.day[1] | 
-                                            Shift == shifts.1.one.day[2])
-        } else{
-          one.shift <- one.day %>% filter(Shift == shifts.one.day[k] | 
-                                            Shift == shifts.1.one.day[1])
-        }
-      } else {
-        one.shift <- one.day %>% filter(Shift == shifts.one.day[k])
-      }
+    
+    for (k1 in 1:length(shifts)){
+      one.day %>%
+        filter(Minutes_since_0000 >= start.shift[k1] &
+                 Minutes_since_0000 <= end.shift[k1]) -> one.shift
       
       if (nrow(one.shift) != 0){
-        out.df[out.df$Date == all.dates[d] & 
-               out.df$Shift == shifts.one.day[k], "Effort"] <- max(one.shift$Minutes_since_0000, 
-                                                                   na.rm = T) - 
-          min(one.shift$Minutes_since_0000, 
-              na.rm = T)
+        # add one row at the top and end of one.shift, so that it has
+        # Event 1 at the top, and 5 at the bottom if they are not there:
+        if (one.shift[1,"Event"] != 1){
+          one.shift.eft <- rbind(one.shift[1,], one.shift)
+          one.shift.eft[1, "Event"] <- 1
+        } else {
+          one.shift.eft <- one.shift
+        }
         
-        out.df[out.df$Date == all.dates[d] & 
-                 out.df$Shift == shifts.one.day[k], "Mother_Calf"] <- sum(one.shift$Mother_Calf, na.rm = T) 
+        if (one.shift[nrow(one.shift), "Event"] != 5){
+          one.shift.eft <- rbind(one.shift.eft, one.shift[nrow(one.shift),])
+          one.shift.eft[nrow(one.shift.eft), "Event"] <- 5
+        }
 
-        out.df[out.df$Date == all.dates[d] & 
-                 out.df$Shift == shifts.one.day[k], "Sea_State"] <- max(one.shift$SeaState, na.rm = T) 
+        # find out how many on/off effort existed
+        row.1 <- which(one.shift.eft$Event == 1)
+        row.5 <- which(one.shift.eft$Event == 5)
         
-        out.df[out.df$Date == all.dates[d] & 
-                 out.df$Shift == shifts.one.day[k], "Vis"] <- max(one.shift$Vis, na.rm = T) 
+        # calculate effort for each on period per shift
+        tmp.eft <- 0
+        for (k2 in 1:length(row.1)){
+          tmp <- one.shift.eft[row.1[k2]:row.5[k2],] 
+          tmp.eft <- tmp.eft + (max(tmp$Minutes_since_0000) - min(tmp$Minutes_since_0000))  
+        }
+
+        out.df[out.df$Date.char == all.dates[d] & 
+                 out.df$Shift == shifts[k1], "Effort"] <- tmp.eft
         
-        out.df[out.df$Date == all.dates[d] & 
-                 out.df$Shift == shifts.one.day[k], "Time_T0"] <- min(one.shift$Minutes_since_T0, 
-                                                                      na.rm = T) 
-        out.df[out.df$Date == all.dates[d] & 
-                 out.df$Shift == shifts.one.day[k], "Time_0000"] <- min(one.shift$Minutes_since_0000, 
-                                                                        na.rm = T) 
+        out.df[out.df$Date.char == all.dates[d] & 
+                 out.df$Shift == shifts[k1], "Mother_Calf"] <- sum(one.shift$Mother_Calf, 
+                                                                  na.rm = T) 
+        
+        out.df[out.df$Date.char == all.dates[d] & 
+                 out.df$Shift == shifts[k1], "Sea_State"] <- max(one.shift$SeaState, 
+                                                                na.rm = T) 
+        
+        out.df[out.df$Date.char == all.dates[d] & 
+                 out.df$Shift == shifts[k1], "Vis"] <- max(one.shift$Vis, na.rm = T) 
+        
+        out.df[out.df$Date.char == all.dates[d] & 
+                 out.df$Shift == shifts[k1], "Time_T0"] <- min(one.shift$Minutes_since_T0, 
+                                                              na.rm = T) 
+        out.df[out.df$Date.char == all.dates[d] & 
+                 out.df$Shift == shifts[k1], "Time_0000"] <- min(one.shift$Minutes_since_0000, 
+                                                                na.rm = T) 
       }
-      
     }
     
   }
@@ -278,3 +308,63 @@ find.effort <- function(x, max.shift){
   return(out.df.1)
 }
 
+format.output <- function(data.shift, max.shift){
+  # create a data frame with the full set of date/shift for dates with observations
+  day.shifts <- data.frame(Date.char = rep(unique(data.shift$Date.char),
+                                           each = max.shift),
+                           Shift = ifelse(max.shift == 4, 
+                                          rep(c("1", "2", "3", "4"), 
+                                              times = length(unique(data.shift$Date.char))),
+                                          rep(c("1", "2", "3", "4", "5"), 
+                                              times = length(unique(data.shift$Date.char)))))
+  # Combine the full set of date/shift with the observed - some shifts are NAs because
+  # they were not in the dataset
+  day.shifts %>% 
+    left_join(data.shift, by = c("Date.char", "Shift")) -> all.day.shifts
+  
+  
+  # create a vector with sequential weeks
+  all.weeks <- seq.Date(as.Date(min(data.shift$Date.char)),
+                        as.Date(max(data.shift$Date.char)),
+                        by = "week")
+  
+  # select necessary data from per-shift data frame, mutate the column names
+  # the create a new data frame
+  data.shift %>% 
+    select(Shift, Date.char, Date.date, Effort, Mother_Calf) %>%
+    mutate(Date.char = Date.char,
+           Effort = Effort/60,
+           Sightings = Mother_Calf) %>%
+    select(Date.char, Shift, Effort, Sightings) %>%
+    mutate(Date.date = as.Date(Date.char)) %>%
+    data.frame() -> raw.data
+  
+  # Create all dates, including weekends
+  all.dates <- seq.Date(as.Date(min(data.shift$Date.char)),
+                        as.Date(max(data.shift$Date.char)),
+                        by = "day")
+  
+  # create all shists, incluyding nights
+  all.shifts <- data.frame(Date.date = rep(all.dates,
+                                           each = 8),
+                           Shift = rep(c("1", "2", "3", "4",
+                                         "5", "6", "7", "8"), 
+                                       times = length(all.dates),
+                                       by = "day"))
+  
+  all.shifts %>% 
+    left_join(raw.data, by = c("Date.date", "Shift")) %>%
+    mutate(Week = ceiling(difftime(Date.date, min(all.dates), 
+                                   units = "weeks")) %>%
+             as.numeric(),
+           Date = Date.date) %>%
+    select(Week, Date, Shift, Effort, Sightings) -> formatted.all.data
+  
+  # need to change week = 0 to week = 1...
+  formatted.all.data[formatted.all.data$Week == 0, "Week"] <- 1
+  
+  # Change NAs to 0s
+  formatted.all.data[is.na(formatted.all.data)] <- 0
+  
+  return(formatted.all.data)
+}
